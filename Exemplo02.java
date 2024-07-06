0e9ffaa0dc99009cd085472dce01d4074a580f2b
Vitória <153361694+vitoriagab9@users.noreply.github.com>
2024-07-06 14:26:14 -0300

    Update Exemplo02.java

--- Exemplo02.java
+++ Exemplo02.java
@@ -14,3 +14,17 @@ public class Exemplo02 {
         }
     }
 }
+
+public class Example02 {
+  public static void main(String[] args) {
+    int result = sum(5, 10);
+    System.out.println(result);
+  }
+  public static int sum(int start, int end) {
+    if (end > start) {
+      return end + sum(start, end - 1);
+    } else {
+      return end;
+    }
+  }
+}
