3bcd1dfe9592e70ad14d9885faa2fe0b74d96d0a
Vitória <153361694+vitoriagab9@users.noreply.github.com>
2024-07-08 13:04:10 -0300

    Update and rename Exemplo02.java to Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+
+    // Função recursiva para calcular o número de Fibonacci de forma eficiente
+    public static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 3; // Exemplo: calcular Fibonacci de 3
+
+        System.out.println("Fibonacci de " + n + " (recursivo): " + fibonacci(n));
+    }
+}
+
